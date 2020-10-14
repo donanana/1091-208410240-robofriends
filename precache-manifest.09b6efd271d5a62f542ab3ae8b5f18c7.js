31174e9166448132befcb1f52bba7102494cb752
donanana <sunghajung28i39@gmail.com>
2020-10-14 19:06:13 +0800

    Updates

---
+++ precache-manifest.09b6efd271d5a62f542ab3ae8b5f18c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77cebdab54e06767bbe86b6e5549d33",
+    "url": "/1091-id-robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-id-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cdf95de2139ddfeece5a",
+    "url": "/1091-id-robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/1091-id-robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1091-id-robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf95de2139ddfeece5a",
+    "url": "/1091-id-robofriends/static/js/main.76d29644.chunk.js"
+  },
+  {
+    "revision": "a51abbcf008854cbf355",
+    "url": "/1091-id-robofriends/static/js/runtime-main.7f4955e0.js"
+  }
+]);
